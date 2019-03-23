0c26cf65d96bbc67e1389316e455f60f4078a430
Rafay Khan <rafayak16@gmail.com>
2019-03-23 13:24:07 +0500

    Solve Dialy Coding Problem #13

---
+++ DailyCodingProblem/13_Amazon_LongestDistinctSubstring.py
@@ -0,0 +1,38 @@
+'''
+This problem was asked by Amazon.
+
+Given an integer k and a string s, find the length of the longest substring that
+contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+longest_substring('abcba', 2)  # longest substring with at most 2 distinct characters is 'bcb'
+    3
+longest_substring('edabccccdccba', 3)  # 'bccccdccb'
+    9
+'''
+
+from collections import deque
+
+def longest_substring(string, k):
+    substring = deque()
+    len_longest_substr = 0
+    idx = 0
+
+    if k == 0:
+        return 0
+    else:
+        while idx < len(string):
+            substring.append(string[idx])
+            if len(set(substring)) == k:
+                if len(substring) > len_longest_substr:
+                    len_longest_substr = len(substring)
+            elif len(set(substring)) > k:
+                substring.popleft()
+            idx+=1
+    return len_longest_substr
+
+
+if __name__ == '__main__':
+    string = 'edabccccdccba'
+    print(longest_substring(string,3))
