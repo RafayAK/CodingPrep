1757804d8c1dad7cf12ec7750ee2fcf11cf7e712
Rafay Khan <rafayak16@gmail.com>
2019-10-10 10:33:20 +0500

    Quick and dirty solution for DCP#72

--- DailyCodingProblem/72_Google_Largest_Path_In_Directed_Graph.py
+++ DailyCodingProblem/72_Google_Largest_Path_In_Directed_Graph.py
@@ -29,4 +29,101 @@ A
 [(0, 0)]
 Should return null, since we have an infinite loop.
 """
+import copy
+class graph_node:
+    def __init__(self, name):
+        self.name = name
+        self.paths = []
+        self.VISITED = False
+
+
+
+def create_nodes(nodes:str):
+    letters = dict() # to keep track of num of times a letter appears, then use count to name node appropriately
+
+    labeled_nodes = dict()
+    labels = []
+    for node in nodes:
+        if node not in letters:
+            letters[node] = 0
+        else:
+            letters[node] += 1
+        # labeled_nodes.append("{}{}".format(node,letters[node]))  # nodes are in form A0, B0, A1, C0, A2
+        gn = graph_node(name=node)
+        labeled_nodes["{}{}".format(node, letters[node])] = gn
+        labels.append("{}{}".format(node, letters[node]))
+    return labeled_nodes, labels
+
+
+def create_adj_matrix(labeled_nodes, labels, edge_list):
+    adj_matrix = {}
+
+    for start, end in edge_list:
+        labeled_nodes[labels[start]].paths.append(labels[end])
+
+        if labels[start] not in adj_matrix:
+            adj_matrix[labels[start]] = []
+
+        adj_matrix[labels[start]].append(labels[end])
+
+    return labeled_nodes, adj_matrix
+
+
+
+def max_path(labeled_nodes, adj_mat:dict):
+
+    # helper returns the most frequent node in the path
+    def helper(start_node, next_nodes, frequency_dict, labeled_nodes):
+        if labeled_nodes[start_node].VISITED is False:
+            labeled_nodes[start_node].VISITED = True
+        else:
+            return None  # found infinite loop
+
+
+        temp_dict = {}
+
+        if labeled_nodes[start_node].name in frequency_dict:
+            frequency_dict[labeled_nodes[start_node].name] += 1
+        else:
+            temp_dict[labeled_nodes[start_node].name]=1
+
+        if start_node not in adj_mat:
+            combined_dict = dict(**temp_dict,**frequency_dict)
+            return max(combined_dict.values())
+
+        max_frequency = 0
+        for node in next_nodes:
+            nn = [] if node not in adj_mat else adj_mat[node]
+            frequency = helper(node, nn, dict(**temp_dict,**frequency_dict), labeled_nodes)
+            if frequency is None:
+                return None
+            max_frequency = max(max_frequency, frequency)
+
+        return max_frequency
+
+    max_path_len = 0
+    for node, paths in adj_mat.items():
+        path_len = helper(start_node=node, next_nodes=paths, frequency_dict={}, labeled_nodes=copy.deepcopy(labeled_nodes))
+
+        if path_len is None:
+            return None
+
+        max_path_len = max(max_path_len, path_len)
+
+    return max_path_len
+
+
+def find_longest_path(nodes, edge_list):
+    # create the nodes. Note each capital letter is a separate node even if has same identifier
+    #   eg.'AA'-> node_A, node_A1
+    labeled_nodes, labels = create_nodes(nodes)
+
+    # create adjacency matrix for the directed graph
+    labeled_nodes, adj_mat = create_adj_matrix(labeled_nodes, labels, edge_list)
+
+    return max_path(labeled_nodes, adj_mat)
+
+if __name__ == '__main__':
+    print(find_longest_path('ABACA', edge_list=[(0, 1), (0, 2), (2, 3), (3, 4)]))
+    print(find_longest_path('A', edge_list=[(0, 0)]))
 
