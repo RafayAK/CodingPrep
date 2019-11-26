d42797a0fc455baf9a661799e75925ac11736f24
Rafay Khan <rafayak16@gmail.com>
2019-11-26 23:42:04 +0500

    add reference DCP#91

--- DailyCodingProblem/91_Dropbox_Fix_Lambda_Expression.py
+++ DailyCodingProblem/91_Dropbox_Fix_Lambda_Expression.py
@@ -10,7 +10,7 @@ for i in range(10):
 for f in functions:
     print(f())
 """
-## https://docs.python.org/3/faq/programming.html#why-do-lambdas-defined-in-a-loop-with-different-values-all-return-the-same-result
+# https://docs.python.org/3/faq/programming.html#why-do-lambdas-defined-in-a-loop-with-different-values-all-return-the-same-result
 
 functions = []
 for i in range(10):
