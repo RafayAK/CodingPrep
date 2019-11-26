8c00d438806e5bc4315df9eb7125639489ab1c4e
Rafay Khan <rafayak16@gmail.com>
2019-11-26 22:40:56 +0500

    Solve Daily Coding Problem#91

---
+++ DailyCodingProblem/91_Dropbox_Fix_Lambda_Expression.py
@@ -0,0 +1,20 @@
+"""
+This problem was asked by Dropbox.
+
+What does the below code snippet print out? How can we fix the anonymous functions to behave as we'd expect?
+
+functions = []
+for i in range(10):
+    functions.append(lambda : i)
+
+for f in functions:
+    print(f())
+"""
+## https://docs.python.org/3/faq/programming.html#why-do-lambdas-defined-in-a-loop-with-different-values-all-return-the-same-result
+
+functions = []
+for i in range(10):
+    functions.append(lambda n=i: n)
+
+for f in functions:
+    print(f())
