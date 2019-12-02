70e7356058eb3a7b4fdeb8f3c0387babb2c6985c
Rafay Khan <rafayak16@gmail.com>
2019-12-02 15:31:57 +0500

    Solve Daily Coding Problem # 95

--- DailyCodingProblem/95_Palantir_Next_Greater_Permutation.py
+++ DailyCodingProblem/95_Palantir_Next_Greater_Permutation.py
@@ -12,23 +12,38 @@ Can you perform the operation without allocating extra memory (disregarding the 
 """
 
 def next_permutation(arr):
-    descending = True
-    d = None  # index of number
-    for i in range(1, len(arr)):
-        if arr[i-1] > arr[i]:
-            descending = False
-            d = i - 1
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] > arr[i-1]:
             break
 
+    if i == 0:
+        return arr[::-1]
+    else:
+        # get the index smallest value to the right of i-1 larger then
+        # the value at index i-1 and swap them
+        smallest_idx = arr[i:].index(min(filter(lambda x:x>arr[i-1], arr[i:]))) + i
+        arr[smallest_idx], arr[i-1] = arr[i-1], arr[smallest_idx]
+        arr[i:] = sorted(arr[i:])
 
-    if descending:
-        arr[-1], arr[-2] = arr[-2], arr[-1]
         return arr
 
 
-
-
-
 if __name__ == '__main__':
-    print(next_permutation([1, 3, 2]))
+    arr1 = [1,2,3]
+    print("Starting permutation: {}".format(arr1))
 
+    next_perm = next_permutation(arr1.copy())
+    while next_perm != arr1:
+        print(next_perm)
+        next_perm = next_permutation(next_perm)
+    print(next_perm)
+
+    print("\n ----------- \n")
+    arr1 = [1,2,3,4]
+    print("Starting permutation: {}".format(arr1))
+
+    next_perm = next_permutation(arr1.copy())
+    while next_perm != arr1:
+        print(next_perm)
+        next_perm = next_permutation(next_perm)
+    print(next_perm)
