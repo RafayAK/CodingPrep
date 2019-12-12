f5d44c9d2bf88ff9878b5373e8387f08778da3ee
Rafay Khan <rafayak16@gmail.com>
2019-12-12 13:28:20 +0500

    Solve Daily Coding Problem # 100

--- DailyCodingProblem/100_Google_Find_Min_Steps_to_Reach_Point.py
+++ DailyCodingProblem/100_Google_Find_Min_Steps_to_Reach_Point.py
@@ -22,47 +22,28 @@ Output: 2
 It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
 """
 
-# idea: use euclidean distance to see if you have moved close to the point or away
+# One way to reach form a point (x1,y1) to (x2, y2) is to move abs(x2-x1)
+# steps in horizontal direction and abs(y2-y1) steps in vertical direction,
+# but this is not the shortest path to reach (x2,y2).
+# The best way would be to cover the maximum possible distance in diagonal direction
+# and remaining in horizontal or vertical direction. If we look closely this just
+# reduces to maximum of abs(x2-x1) and abs(y2-y1).
+#
+# Example
+# x1 = 5, y1= 20
+# x2 = 15, y2 = 15
+#
+# we first move diagonally to reach (10,15) this takes 5 steps and then we move 5 units in x direction,
+# which again takes 5 steps. In total this is 10 steps which is equal to MAX(abs(15-5), abs(15-20))
 
-def squared_euclidean_dist(point_A, point_B):
-    return (point_A[0]-point_B[0])**2 + (point_A[1] - point_B[1])**2
-
-moves = [
-    (+1, 0),
-    (-1, 0),
-    (0, +1),
-    (0, -1),
-    (-1, -1),
-    (+1, +1),
-    (-1, +1),
-    (+1, -1)
-]
 
 
 def find_min_steps(sequence):
-    def helper(point_a, point_b, prev_dist, min_steps=0):
-        if prev_dist == 0:
-            return min_steps
-
-        current_dist = squared_euclidean_dist(point_a, point_b)
-        if current_dist > prev_dist:
-            return float('inf')
-
-
-        # try all moves select the shortest move
-        steps = []
-        for move in moves:
-            temp_point = (point_a[0]+move[0], point_b[1]+point_b[1])
-            steps = helper(temp_point, point_b, current_dist, min_steps+1)
-
-        return min(steps)
-
 
     min_steps = 0
     a = sequence[0]
     for b in sequence[1:]:
-        # find shortest_path from a to b
-        min_steps += helper(point_a=a, point_b=b, prev_dist=squared_euclidean_dist(a, b))
+        min_steps += max(abs(b[0]-a[0]), abs(b[1] - a[1]))
         a = b
 
     return min_steps
@@ -70,4 +51,5 @@ def find_min_steps(sequence):
 
 
 if __name__ == '__main__':
-    print(find_min_steps([(0, 0), (1, 1), (1, 2)]))
+    print(find_min_steps([(0, 0), (1, 1), (1, 2)]))  # 2
+    print(find_min_steps([(4, 6), (1, 2), (4, 5), (10, 12)]))  # 14
