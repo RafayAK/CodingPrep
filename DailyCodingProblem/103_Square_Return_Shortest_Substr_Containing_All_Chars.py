d000e03c70963615ccbb90e37ec1e8563418765c
Rafay Khan <rafayak16@gmail.com>
2019-12-16 17:09:19 +0500

    Solve Daily Coding Problem # 103 with a better solution

--- DailyCodingProblem/103_Square_Return_Shortest_Substr_Containing_All_Chars.py
+++ DailyCodingProblem/103_Square_Return_Shortest_Substr_Containing_All_Chars.py
@@ -25,6 +25,43 @@ def get_shortest_str_with_all_chars(string, chars):
     return None
 
 
+# Basically, creates a window of increasing size to the right until the 'char' set is a subset of the 'temp_set'
+# then, succescively reduces the window from the left until the char set is no longer part of temp_set, this is a
+# candidate window for the shortest window, store location in "final_start and final_end". Try finding another window
+# if possible, by setting "start" to the ending index of the previous window and "end" a step back to reset the window.
+def get_shortest_str_redux(string, chars:set):
+    final_start= None
+    final_end = None
+
+    start, end = 0, 0
+    temp_set = set()
+    while end < len(string):
+
+        temp_set.add(string[end])
+
+        if chars.issubset(temp_set):
+            while chars.issubset(temp_set):
+                temp_set.discard(string[start])
+                start += 1
+            start -= 1
+            if final_end is None:
+                final_start = start
+                final_end = end
+            elif final_end - final_start > end-start:
+                final_start = start
+                final_end = end
+
+            start = end
+            end -= 1
+            temp_set = set()
+
+
+        end += 1
+
+    return string[final_start:final_end+1] if final_start is not None else None
+
+
+
 
 if __name__ == '__main__':
     print(get_shortest_str_with_all_chars("figehaeci", {'a', 'e', 'i'}))
@@ -34,3 +71,13 @@ if __name__ == '__main__':
     print(get_shortest_str_with_all_chars("abcdecdb", {'b', 'c'}))
     print(get_shortest_str_with_all_chars("abcdecdb", {'b', 'c', 'e'}))
     print(get_shortest_str_with_all_chars("abcdecdb", {'x', 'y', 'z'}))
+
+    print('\n ---- \n')
+
+    print(get_shortest_str_redux("figehaeci", {'a', 'e', 'i'}))
+    print(get_shortest_str_redux("abccbbbccbcb", {'a', 'b', 'c'}))
+    print(get_shortest_str_redux("abcdedbc", {'d', 'b', 'b'}))
+    print(get_shortest_str_redux("abcdedbc", {'b', 'c'}))
+    print(get_shortest_str_redux("abcdecdb", {'b', 'c'}))
+    print(get_shortest_str_redux("abcdecdb", {'b', 'c', 'e'}))
+    print(get_shortest_str_redux("abcdecdb", {'x', 'y', 'z'}))
