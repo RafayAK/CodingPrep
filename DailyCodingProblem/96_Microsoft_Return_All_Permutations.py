72263c461f7e7dbe327bb14e63630b1a2efba532
Rafay Khan <rafayak16@gmail.com>
2019-12-17 12:46:32 +0500

    Add better solution to DCP # 96

--- DailyCodingProblem/96_Microsoft_Return_All_Permutations.py
+++ DailyCodingProblem/96_Microsoft_Return_All_Permutations.py
@@ -22,6 +22,8 @@ def next_permutation(arr):
 
         return arr
 
+
+# this uses the solution from DCP # 95
 def all_permutations(arr):
     result = []
 
@@ -33,6 +35,29 @@ def all_permutations(arr):
 
     return result
 
+# better than the previous sol, simpler
+def all_permutations_redux(arr):
+    perms = []
+    def helper(arr, start=0):
+        len_arr = len(arr)
+        if len_arr - 1 == start:
+            perms.append(arr)
+            return
+
+        for i in range(start, len_arr):
+            new_arr = arr.copy()
+            new_arr[start], new_arr[i] = new_arr[i], new_arr[start]
+            helper(new_arr, start+1)
+
+    helper(arr)
+    return perms
+
+
 if __name__ == '__main__':
     print(all_permutations([1,2,3]))
     print(all_permutations([3,2,1]))
+
+    print("\n ---- \n")
+
+    print(all_permutations_redux([1,2,3]))
+    print(all_permutations_redux([3,2,1]))
