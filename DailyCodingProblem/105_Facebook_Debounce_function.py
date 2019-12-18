ed44799f8e01336bde2bd93025bf70f2d6d85a6b
Rafay Khan <rafayak16@gmail.com>
2019-12-18 23:15:54 +0500

    Add file for DCP # 105

---
+++ DailyCodingProblem/105_Facebook_Debounce_function.py
@@ -0,0 +1,10 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Facebook.
+
+Given a function f, and N return a debounced f of N milliseconds.
+
+That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.
+"""
+
