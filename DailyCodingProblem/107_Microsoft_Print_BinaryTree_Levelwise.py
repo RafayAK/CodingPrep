33d08508ca238a30742c6f48e7e99f57cc7a0a8a
Rafay Khan <rafayak16@gmail.com>
2019-12-24 13:32:42 +0500

    Solve Daily Coding Problem # 107

--- DailyCodingProblem/107_Microsoft_Print_BinaryTree_Levelwise.py
+++ DailyCodingProblem/107_Microsoft_Print_BinaryTree_Levelwise.py
@@ -9,3 +9,30 @@ Print the nodes in a binary tree level-wise. For example, the following should p
    / \
   4   5
 """
+
+class node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def print_level_wise(root, l=0, nodes=[]):
+    try:
+        nodes[l].append(root.data)
+    except:
+        nodes.append([])
+        nodes[l].append(root.data)
+
+    if root.left:
+        nodes = print_level_wise(root.left, l+1, nodes)
+
+    if root.right:
+        nodes = print_level_wise(root.right, l + 1, nodes)
+
+    return nodes
+
+
+if __name__ == '__main__':
+    tree = node(1, left=node(2),
+                right=node(3, left=node(4), right=node(5)))
+    print(*print_level_wise(tree), sep='\n')
