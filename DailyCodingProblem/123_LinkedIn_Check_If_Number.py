27e93e561794682bb5f52dbf1e984512339c7d57
Rafay Khan <rafayak16@gmail.com>
2020-01-11 14:15:13 +0500

    Better implementation of DCP#123

--- DailyCodingProblem/123_LinkedIn_Check_If_Number.py
+++ DailyCodingProblem/123_LinkedIn_Check_If_Number.py
@@ -27,21 +27,65 @@ def is_num(string):
 
     return True
 
+
+def is_num_redux(string:str):
+    return is_positive_number(string) or is_negative_number(string) or is_scientific_number(string)
+
+
+def is_scientific_number(s:str):
+    if s.count('e') != 1:
+        return False
+
+    before_e, after_e = s.split('e')
+
+    return (is_positive_number(before_e) or is_negative_number(before_e)) and \
+           (is_positive_number(after_e) or is_negative_number(after_e))
+
+
+def is_positive_number(s:str):
+    return is_positive_int(s) or is_positive_real(s)
+
+
+def is_negative_number(s:str):
+    return is_negative_int(s) or is_negative_real(s)
+
+
+def is_negative_real(s:str):
+    return s.startswith('-') and is_positive_real(s[1:])
+
+
+def is_positive_real(s:str):
+    if s.count('.') != 1:
+        return False
+
+    integer_part, decimal_part = s.split(".")
+
+    return is_positive_number(integer_part) and is_positive_number(decimal_part)
+
+
+def is_negative_int(s:str):
+    return s.startswith('-') and is_positive_int(s[1:])
+
+
+def is_positive_int(s:str):
+    return s.isdigit()
+
+
 if __name__ == '__main__':
-    print(is_num("10"))
-    print(is_num("-10"))
-    print(is_num("10.1"))
-    print(is_num("-10.1"))
-    print(is_num("1e5"))
-    print(is_num("1e-5"))
 
 
-    print("\n------")
-    print(is_num("a"))
-    print(is_num("x 1"))
-    print(is_num("a -2"))
-    print(is_num("-"))
-
+    print(is_num_redux("10"))
+    print(is_num_redux("-10"))
+    print(is_num_redux("10.1"))
+    print(is_num_redux("-10.1"))
+    print(is_num_redux("1e5"))
+    print(is_num_redux("1e-5"))
+    print(is_num_redux("1e-5.2"))
+    print()
+    print(is_num_redux("a"))
+    print(is_num_redux("x 1"))
+    print(is_num_redux("a -2"))
+    print(is_num_redux("-"))
 
 
 
