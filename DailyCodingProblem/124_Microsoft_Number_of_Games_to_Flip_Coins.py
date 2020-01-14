5d33dfeaaa3b7b2eca345b226038700606695409
Rafay Khan <rafayak16@gmail.com>
2020-01-14 09:25:04 +0500

    Solve Daily Coding Problem # 124

--- DailyCodingProblem/124_Microsoft_Number_of_Games_to_Flip_Coins.py
+++ DailyCodingProblem/124_Microsoft_Number_of_Games_to_Flip_Coins.py
@@ -6,3 +6,26 @@ up heads you flip again. How many rounds do you expect to play before only one c
 
 Write a function that, given n, returns the number of rounds you'd expect to play until one coin remains.
 """
+
+
+# There is a probability that after each round the number of conis remainning will be halved
+
+def rounds_until_one(coins):
+    if coins <= 0:
+        return None
+
+    if coins == 1:
+        return 0
+
+    round_number = rounds_until_one(coins//2) + 1
+    print("     {0} coins on round {1}".format(coins, round_number))
+    return round_number
+
+
+
+if __name__ == '__main__':
+    coins = [100, 50, 25, 0, -1]
+
+    for c in coins:
+        print("Number of rounds played: {}".format(rounds_until_one(c)))
+
