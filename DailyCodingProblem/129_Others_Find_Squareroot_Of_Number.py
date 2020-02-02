bf93677030016de68647e8c08781d853c21246d0
Rafay Khan <rafayak16@gmail.com>
2020-02-02 10:35:30 +0500

    Add another solution for DCP#129

--- DailyCodingProblem/129_Others_Find_Squareroot_Of_Number.py
+++ DailyCodingProblem/129_Others_Find_Squareroot_Of_Number.py
@@ -33,7 +33,29 @@ def get_square_root_redux(num, error=0.00001):
 
     return guess
 
+
+
+def get_square_root_redux_redux(num, learning_rate=0.0001, epochs=5000):
+    # a little ML ;)
+
+    guess = 1
+    Loss_function = lambda a: (1/2) * (a-num)**2
+    dLoss = lambda a: a*(a-num)
+
+    for i in range(epochs):
+        if i % 1000==0 or i==epochs-1:
+            print(">>>>> Loss: {}".format(Loss_function(guess**2)))
+
+        dL = dLoss(guess**2)
+        dx = dL * (2*guess)
+
+        guess = guess - learning_rate*dx
+
+    return guess
+
+
 if __name__ == '__main__':
     print(get_square_root(9))
     print(get_square_root_redux(9))
+    print(get_square_root_redux_redux(9, epochs=8000))
 
