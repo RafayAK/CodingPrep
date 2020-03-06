b849000616ac34fb5e3080b4bc20ed6f2aaf2543
Rafay Khan <rafayak16@gmail.com>
2020-03-06 09:31:47 +0500

    Add file for DCP # 138

--- DailyCodingProblem/138_Google_Coin_Change_Problem.py
+++ DailyCodingProblem/138_Google_Coin_Change_Problem.py
@@ -10,6 +10,14 @@ For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and
 
 # classic memoization problem
 
-memo_table = {1:1, 5:1, 10:1, 25:1}
+memo_table = []
+
+def create_table(denominations,s):
+    for coin in denominations:
+        for sum in s:
+            memo_table[]
+
+
 
 def min_coins_required(n):
+    create_table
