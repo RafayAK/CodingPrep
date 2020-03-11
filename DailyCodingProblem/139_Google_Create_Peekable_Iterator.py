73909038d84643512df990f69e4f52596a0e1e97
Rafay Khan <rafayak16@gmail.com>
2020-03-11 10:54:25 +0500

    Solve Daily Coding Problem # 139

--- DailyCodingProblem/139_Google_Create_Peekable_Iterator.py
+++ DailyCodingProblem/139_Google_Create_Peekable_Iterator.py
@@ -20,13 +20,36 @@ class PeekableInterface(object):
 
 class PeekableInterface(object):
     def __init__(self, iterator):
-        pass
+        self.iterator = iterator
+        self._next = next(iterator)
 
     def peek(self):
-        pass
+        return self._next
 
     def next(self):
-        pass
+        result = self._next
+        try:
+            self._next = next(self.iterator)
+        except StopIteration:
+            self._next = None
+        return result
 
     def hasNext(self):
-        pass
+        return self._next is not None
+
+
+if __name__ == '__main__':
+    lst = [1, 2, 3, 4, 5]
+
+    peekable_lst = PeekableInterface(iter(lst))
+
+    assert peekable_lst.peek() == 1
+    assert peekable_lst.hasNext()
+    assert peekable_lst.next() == 1
+
+    assert peekable_lst.next() == 2
+    assert peekable_lst.next() == 3
+    assert peekable_lst.next() == 4
+    assert peekable_lst.next() == 5
+
+    assert not peekable_lst.hasNext()
