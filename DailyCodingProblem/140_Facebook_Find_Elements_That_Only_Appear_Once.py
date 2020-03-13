bc2e325b78a97219c9034964ac69874c618bb392
Rafay Khan <rafayak16@gmail.com>
2020-03-13 11:49:57 +0500

    Solve Daily Coding Problem # 140

--- DailyCodingProblem/140_Facebook_Find_Elements_That_Only_Appear_Once.py
+++ DailyCodingProblem/140_Facebook_Find_Elements_That_Only_Appear_Once.py
@@ -16,10 +16,30 @@ def occur_once(array):
     for num in array[1:]:
         xored_sum = xored_sum ^ num
 
-    # Todo: make this O(n) currently O(n^2)
+    # make this O(n) currently O(n^2)
+    # for num in array:
+    #     if xored_sum ^ num in array:
+    #         return xored_sum ^ num, num
+
+    # Now we have to find xored_sum is the sum of which two numbers?
+    # idea: the numbers in xored_sum can be divided into two groups:
+    #       1. the number with the most significant bit(MSB) => 1
+    #       2. the number with the MSB => 0
+    #
+    # find the MSB from the right:
+    msb = xored_sum & (xored_sum-1)
+
+    # res_1 by the end will contain the number from group 2 that satisfies the condition, similarly res_2 will contain the number from group 2
+    res_1, res_2 = 0, 0
     for num in array:
-        if xored_sum ^ num in array:
-            return xored_sum ^ num, num
+        if num & msb == 0:
+            res_1 ^= num
+        else:
+            res_2 ^= num
+
+    return res_1, res_2
+
 
 if __name__ == '__main__':
-    print(occur_once([2, 4, 6, 8, 10, 2, 6, 10]))
+    assert occur_once([2, 4, 6, 8, 10, 2, 6, 10]) == (4, 8)
+    assert occur_once([12 , 2, 4, 6, 8, 10, 2, 6, 10, 12]) == (4, 8)
