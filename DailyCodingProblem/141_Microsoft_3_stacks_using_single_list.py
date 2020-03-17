b1e6ce2b4c0bdf735661c8365a6cf159183d7649
Rafay Khan <rafayak16@gmail.com>
2020-03-17 23:08:22 +0500

    Add initial class

---
+++ DailyCodingProblem/141_Microsoft_3_stacks_using_single_list.py
@@ -0,0 +1,29 @@
+"""
+This problem was asked by Microsoft.
+
+Implement 3 stacks using a single list:
+
+class Stack:
+    def __init__(self):
+        self.list = []
+
+    def pop(self, stack_number):
+        pass
+
+    def push(self, item, stack_number):
+        pass
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.list = []
+
+        self.stack_markers = [(0,0), (0,0), (0,0)]
+
+    def pop(self, stack_number):
+        pass
+
+
+    def push(self, item, stack_number):
+        pass
