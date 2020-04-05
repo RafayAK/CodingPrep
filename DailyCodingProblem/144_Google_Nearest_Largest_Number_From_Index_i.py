ea408def1bb7151a3dd2d4e6c5eefde239c8427f
Rafay Khan <rafayak16@gmail.com>
2020-04-05 18:01:05 +0500

    Solve Daily Coding Problem # 144

--- DailyCodingProblem/144_Google_Nearest_Largest_Number_From_Index_i.py
+++ DailyCodingProblem/144_Google_Nearest_Largest_Number_From_Index_i.py
@@ -31,7 +31,7 @@ def index_of_nearest_larger_num(arr, index):
         return larger_index, larger_distance
 
     left_index, left_distance = search(index, left=True)  # search left
-    right_index, right_distance = search(index, left=False)  # serach right
+    right_index, right_distance = search(index, left=False)  # search right
 
     if left_distance < right_distance:
         return left_index
@@ -46,13 +46,46 @@ def index_of_nearest_larger_num_redux(arr, index):
         right = index + i
 
         if left >= 0 and arr[left] > arr[index]:
-            return arr[left], left
+            return left
         if right < len(arr) and arr[right] > arr[index]:
-            return arr[right], right
+            return right
 
     return None
 
-
+"""
+Preprocess works by checking the immediate left and right neighbours
+For example given the arr=[4, 1, 3, 5, 6] we preprocess as follows:
+   1. create cache to store the indices of larger neighbour
+        cache = [None, None, None, None, None]
+   2. iterate over each index checking only its immediate neighbour
+        
+        iter 1: 
+            if      4 > 1 -->> Yes
+                    cache = [None, 0, None, None, None]
+        
+        iter 2:    
+            if      1 > 3 ---> No   
+            elif    3 > 1 ---> Yes
+                    cache = [None, 2, None, None, None]
+        
+        iter 3:
+            if      3 > 5 ---> No
+            elif    5 > 3 ---> Yes
+                    cache = [None, 2, 3, None, None]
+                    
+        iter 4: 
+            if      5 > 6 ---> No
+            elif    6 > 5 ---> Yes
+                    cache = cache = [None, 2, 3, 4, None]      
+                    
+        Now there the None values need to filled in through the O(n) algo
+        After falling back to the O(n) algo the final cache is:
+                    cache = cache = [3, 2, 3, 4, None],  
+                    Note there is no element greater than 6 so its index remains None
+                    
+                    
+        If there are duplicates than the worst case is O(n^2)
+"""
 def preprocess(arr):
     cache = [None for _ in range(len(arr))]
 
@@ -62,33 +95,32 @@ def preprocess(arr):
         elif arr[j +1] > arr[j]:
             cache[j] = j+1
 
+    for i in range(len(cache)):
+        if cache[i] is None:
+            # fall back to the O(n) algo
+            cache[i] = index_of_nearest_larger_num_redux(arr, i)
+
     return cache
 
-def get_mapping_indices(arr):
-    nl_indices = dict()
-    sorted_tuples = [(x, i) for i, x in enumerate(arr)]
-    sorted_tuples.sort(key=lambda x: x[0])
 
-    for k, (_, i) in enumerate(sorted_tuples[:-1]):
-        min_dist = len(arr)
-        for m in range(k + 1, len(sorted_tuples)):
-            dist = abs(i - sorted_tuples[m][1])
-            if dist < min_dist:
-                min_dist = dist
-                nl_indices[i] = sorted_tuples[m][1]
+cache = None  # global cache for pre-processing
 
-    return nl_indices
 
+def return_nearest(arr, i):
+    global cache
+    if cache is None:
+        cache = preprocess(arr)
+    return cache[i]
 
 if __name__ == '__main__':
     # print(index_of_nearest_larger_num([4, 1, 3, 5, 6], 0))
     # print(index_of_nearest_larger_num([4, 1, 3, 5, 6], 1))
     # print(index_of_nearest_larger_num([4, 1, 3, 5, 6], 4))
 
-    print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 0))
-    print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 1))
-    print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 4))
+    # print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 0))
+    # print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 1))
+    # print(index_of_nearest_larger_num_redux([4, 1, 3, 5, 6], 4))
 
-    # print(preprocess([4, 1, 3, 5, 6]))
-    # print(preprocess([1, 1, 1, 4, 3, 3]))
-    print(get_mapping_indices([4, 1, 3, 5, 6]))
+    print(return_nearest([4, 1, 3, 5, 6], 0))
+    print(return_nearest([4, 1, 3, 5, 6], 1))
+    print(return_nearest([4, 1, 3, 5, 6], 4))
