7556825f858893b91cbc41a5d60326cd80bd7723
Rafay Khan <rafayak16@gmail.com>
2020-04-07 17:48:11 +0500

    Add second solution to DCP#107

--- DailyCodingProblem/107_Microsoft_Print_BinaryTree_Levelwise.py
+++ DailyCodingProblem/107_Microsoft_Print_BinaryTree_Levelwise.py
@@ -16,7 +16,10 @@ class node:
         self.left = left
         self.right = right
 
-def print_level_wise(root, l=0, nodes=[]):
+
+def print_level_wise(root, l=0, nodes=None):
+    if nodes is None:  # set default value
+        nodes = []
     try:
         nodes[l].append(root.data)
     except:
@@ -32,7 +35,24 @@ def print_level_wise(root, l=0, nodes=[]):
     return nodes
 
 
+from queue import Queue
+def print_level_wise_redux(root):
+    queue = Queue()
+    queue.put(root)
+
+    while not queue.empty():
+        node = queue.get()
+        if node.left:
+            queue.put(node.left)
+
+        if node.right:
+            queue.put(node.right)
+        print(node.data)
+
+
 if __name__ == '__main__':
     tree = node(1, left=node(2),
                 right=node(3, left=node(4), right=node(5)))
     print(*print_level_wise(tree), sep='\n')
+    print("----")
+    print_level_wise_redux(tree)
