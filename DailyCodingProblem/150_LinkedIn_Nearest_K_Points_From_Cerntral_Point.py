6a7f3d592d6895fbd0b8f63f663f17cf3e96084c
Rafay Khan <rafayak16@gmail.com>
2020-04-14 11:18:48 +0500

    Solve Daily Coding Problem # 150

--- DailyCodingProblem/150_LinkedIn_Nearest_K_Points_From_Cerntral_Point.py
+++ DailyCodingProblem/150_LinkedIn_Nearest_K_Points_From_Cerntral_Point.py
@@ -8,3 +8,16 @@ return [(0, 0), (3, 1)].
 
 
 """
+
+
+def k_nearest(points, central, k):
+    squared_l2_norm = lambda p1, p2 : (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+
+    distances = {p: squared_l2_norm(p, central) for p in points}
+
+    # return the k nearest points after sorting based on distances of points
+    return [p for p,_ in sorted(distances.items(), key=lambda item: item[1])][:k]
+
+
+if __name__ == '__main__':
+    print(k_nearest([(0, 0), (5, 4), (3, 1)], (1, 2), 2))
