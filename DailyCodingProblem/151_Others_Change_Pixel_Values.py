ebbfa0c1e74f27a1f6d6574f106848baea174ecd
Rafay Khan <rafayak16@gmail.com>
2020-04-16 10:36:56 +0500

    Add initial implementation

--- DailyCodingProblem/151_Others_Change_Pixel_Values.py
+++ DailyCodingProblem/151_Others_Change_Pixel_Values.py
@@ -15,3 +15,38 @@ G G G
 G G G
 B B B
 """
+
+# key idea : flood-fill algo with 8 connectivity components
+
+connectivity = [
+    (-1, 0), # North
+    (-1, 1), # North-East
+    (0, 1), # East
+    (1, 1), # South-East
+    (1, 0), # South
+    (1, -1), # South-West
+    (0, -1), # West
+    (-1, -1), # North-West
+]
+
+def my_flood_fill(mat, loc, new_color):
+    r,c = loc
+    curr_color = mat[r][c]
+    mat[r][c] = new_color
+    for row, col in connectivity:
+        if -1 < r+row < len(mat) and -1 < c+col < len(mat[1]) and mat[r+row][c+col] == curr_color:
+                my_flood_fill(mat, (r+row, col+c), new_color)
+
+if __name__ == '__main__':
+    mat = [
+        ['B', 'B', 'W'],
+        ['W', 'W', 'W'],
+        ['W', 'W', 'W'],
+        ['B', 'B', 'B']
+    ]
+
+    print(*mat, sep='\n', end='\n'+'---'*5+'\n')
+
+    my_flood_fill(mat, (2,2), 'G')
+
+    print(*mat, sep='\n', end='\n'+'---'*5+'\n')
