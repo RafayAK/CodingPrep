57d286c3cf83ef344674b03d49fa6a784da954b4
Rafay Khan <rafayak16@gmail.com>
2020-04-24 12:19:30 +0500

    Add initial implementation for DCP # 152

--- DailyCodingProblem/152_Tripplebyte_Genrate_number_with_givin_probablity.py
+++ DailyCodingProblem/152_Tripplebyte_Genrate_number_with_givin_probablity.py
@@ -9,4 +9,51 @@ your function should return 1 10% of the time, 2 50% of the time, and 3 and 4 20
 
 You can generate random numbers between 0 and 1 uniformly.
 """
-# can do it when static but how to scale it?
+# Idea:
+#   1.Sort numbers in descending order according to probability:
+#       eg [1, 2, 3, 4], [0.1, 0.5, 0.2, 0.2] ---sort---> [2, 3, 4, 1], [0.5, 0.2, 0.2, 0.1]
+#   2. Since we can generate uniform random numbers b/w 0 and 1, use this to define when to output which number
+#      eg After sorting:  1.0 - 0.5 = 0.5   --so--> if rand uniform > 0.5 output 2
+#                         0.5 - 0.2 = 0.3   --so--> elif rand uniform > 0.3 output 3
+#                         0.3 - 0.2 = 0.1    --so--> elif rand uniform > 0.1 output 4
+#                         else output 1
+#
+
+import random
+class NumGenerator:
+    def __init__(self, numbers:list, probs:list):
+
+        numbers = [n for n,_ in sorted(zip(numbers, probs), key= lambda pair : pair[1], reverse=True)]
+        probs = sorted(probs, reverse=True)
+        # # create mapping_thresholds
+        temp = 1.0
+        self.mapping = {}
+        for i in range(len(probs)):
+            self.mapping[temp-probs[i]] = numbers[i]
+            temp-=probs[i]
+
+
+
+    def generate(self):
+        uni_rand_num = random.uniform(0.0, 1.0)
+        for thres in self.mapping:
+            if uni_rand_num > thres:
+                return self.mapping[thres]
+
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4]
+    probs =  [0.1, 0.5, 0.2, 0.2]
+    test = NumGenerator(nums, probs)
+
+    num_iterations = 100000
+
+    values = {i:0 for i in nums}
+
+    for j in range(num_iterations):
+        values[test.generate()]+=1
+
+    for k, v in values.items():
+        print("{}: {}".format(k, v/num_iterations))
+
