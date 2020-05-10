d789968b2cf1fac4d3cea568f8feccb20af19160
Rafay Khan <rafayak16@gmail.com>
2020-05-10 14:57:51 +0500

    Solve Daily Coding Problem # 155

--- DailyCodingProblem/155_MongoDB_Find_Majority_ELement_Which_Appears_MoreThatHalfTheTIme.py
+++ DailyCodingProblem/155_MongoDB_Find_Majority_ELement_Which_Appears_MoreThatHalfTheTIme.py
@@ -8,17 +8,21 @@ You can assume that such element exists.
 For example, given [1, 2, 1, 1, 3, 4, 0], return 1.
 """
 
-def majority_element(l):
-    counter_dict = {}
-    for ele in l:
-        if ele not in counter_dict:
-            counter_dict[ele] = 0
+# The example provided is WORNG!
+# according to the second requirement of the majority element > floor(len(lst) / 2.0) = floor(7 /2.0) = floor(3.5) = 3
+# and the frequency of element "1" = 3 not > 3
 
-        counter_dict[ele] += 1
+def majority_element(l):
+    frequency_map = {}
+    for ele in l:
+        if ele not in frequency_map:
+            frequency_map[ele] = 0
+
+        frequency_map[ele] += 1
 
     majority_thresh = len(l) // 2
-
-    for key, value in counter_dict.items():
+    
+    for key, value in frequency_map.items():
         if value > majority_thresh:
             return key
 
@@ -27,3 +31,5 @@ def majority_element(l):
 
 if __name__ == '__main__':
     print(majority_element([1, 2, 1, 1, 3, 4, 0]))
+    print(majority_element([1, 2, 1, 1, 3, 4, 1]))
+    print(majority_element([0]))
