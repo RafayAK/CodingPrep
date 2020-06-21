95d2e89567ebe06940889aa80d5c8d62f67ad3c6
Rafay Khan <rafayak16@gmail.com>
2020-06-21 09:47:25 +0500

    Solve Daily Coding Problem # 162

--- DailyCodingProblem/162_Square_Shortest_Unique_Prefix.py
+++ DailyCodingProblem/162_Square_Shortest_Unique_Prefix.py
@@ -29,3 +29,48 @@ Return the list:
 #     a             ap          apr
 #     f             f           f
 #
+
+import itertools
+
+def get_unique_prefixes(words):
+
+    unique_prefixes = set()
+
+    def all_unique_prefixes(prefix_dict):
+        # this function adds unique prefixes to "unique_prefixes" set
+        # and non-unique into "non_unique_prefixes" and returns them.
+        non_unique_prefixes = {}
+
+        for key, val in prefix_dict.items():
+            if len(val) == 1:
+                unique_prefixes.add(key)
+            else:
+                non_unique_prefixes[key] = val
+
+        return non_unique_prefixes
+
+    def helper(word_list, prefix_len=1):
+        prefix_dict = {}
+        for word in word_list:
+            if word[:prefix_len] not in prefix_dict:
+                prefix_dict[word[:prefix_len]] = []
+            prefix_dict[word[:prefix_len]].append(word)
+
+        non_unique_prefixes = all_unique_prefixes(prefix_dict)
+
+        if len(non_unique_prefixes) > 0:
+            # since simple dict.values() will return list of list in this case eg:
+            # {a:[apple, apricot], c = [car, cat]}.values () = [[apple, apricot], [car, cat]]
+            # we need to flatten this list of list, itertools.chain does that for us if we pass
+            # it decompressed/unpacked(*) lists
+            helper(list(itertools.chain(*non_unique_prefixes.values())), prefix_len=prefix_len+1)
+
+    helper(words)
+
+    return unique_prefixes
+
+
+if __name__ == '__main__':
+    print(get_unique_prefixes(['dog', 'cat', 'apple', 'apricot', 'fish']))
+    print(get_unique_prefixes(['dog', 'cat', 'apple', 'apricot', 'fish', 'car']))
+
