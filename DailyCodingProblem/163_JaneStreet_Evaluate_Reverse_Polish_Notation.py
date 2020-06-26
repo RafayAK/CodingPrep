cad6d80a1e3928cef6442aa115dc196616efc74e
Rafay Khan <rafayak16@gmail.com>
2020-06-26 13:14:28 +0500

    Solve Daily Coding Problem # 163

--- DailyCodingProblem/163_JaneStreet_Evaluate_Reverse_Polish_Notation.py
+++ DailyCodingProblem/163_JaneStreet_Evaluate_Reverse_Polish_Notation.py
@@ -10,3 +10,42 @@ should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 
 
 You can assume the given expression is always valid.
 """
+
+# keep a window of 3, start from the left of the array and perform th operation
+# when the 3rd element of the window contains an arithmetic op
+
+def eval_reverrse_polish(exp:list):
+    start = 0
+    end = start+2
+
+    while isinstance(exp[end], (int, float, complex)) and not isinstance(exp[end], bool):
+        start = start+1
+        end = end+1
+
+    op = exp[end]
+    val = 0
+    if op == "+":
+        # add values
+        val = exp[start] + exp[start+1]
+    elif op == '-':
+        # subtract vals
+        val = exp[start] - exp[start+1]
+    elif op == '/':
+        # divide vals
+        val = exp[start] / exp[start+1]
+    elif op == '*':
+        # multiply vals
+        val = exp[start] * exp[start+1]
+    else:
+        raise Exception("Unknown arithmetic operation : {}".format(op))
+
+    exp[start:end+1] = [val]
+
+    if len(exp) == 1:
+        return exp[0]
+    else:
+        return eval_reverrse_polish(exp)
+
+
+if __name__ == '__main__':
+    print(eval_reverrse_polish([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']))
