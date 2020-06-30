42039a47cb71ccb1b2ca04c76832b84f2e63a8e2
Rafay Khan <rafayak16@gmail.com>
2020-06-30 09:08:12 +0500

    Add better implementation

--- DailyCodingProblem/163_JaneStreet_Evaluate_Reverse_Polish_Notation.py
+++ DailyCodingProblem/163_JaneStreet_Evaluate_Reverse_Polish_Notation.py
@@ -14,7 +14,7 @@ You can assume the given expression is always valid.
 # keep a window of 3, start from the left of the array and perform th operation
 # when the 3rd element of the window contains an arithmetic op
 
-def eval_reverrse_polish(exp:list):
+def eval_reverse_polish(exp:list):
     start = 0
     end = start+2
 
@@ -44,8 +44,39 @@ def eval_reverrse_polish(exp:list):
     if len(exp) == 1:
         return exp[0]
     else:
-        return eval_reverrse_polish(exp)
+        return eval_reverse_polish(exp)
 
+# better implementation
+# solve it with stack
+PLUS = "+"
+MINUS = "-"
+MULTIPLY = "*"
+DIVIDE = "/"
+
+OPS = [PLUS, MINUS, MULTIPLY, DIVIDE]
+
+def eval_reverse_polish_redux(exp:list):
+    stack = []
+
+    for symbol in exp:
+        if symbol in OPS:
+            # take out the most recent to values and apply the operation on them
+            val2, val1 = stack.pop(-1), stack.pop(-1)
+            if symbol == PLUS:
+                stack.append(val1 + val2)
+            elif symbol == MINUS:
+                stack.append(val1 - val2)
+            elif symbol == MULTIPLY:
+                stack.append(val1 * val2)
+            elif symbol == DIVIDE:
+                stack.append(val1 / val2)
+        else:
+            # add to the top of the stack
+            stack.append(symbol)
+
+    return stack[0]
 
 if __name__ == '__main__':
-    print(eval_reverrse_polish([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']))
+    print(eval_reverse_polish([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']))
+
+    print(eval_reverse_polish_redux([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']))
