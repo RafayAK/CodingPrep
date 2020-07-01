5071c6365df63a90f28e026023fad0f4b601a6ed
Rafay Khan <rafayak16@gmail.com>
2020-07-01 13:23:04 +0500

    Add better implementation DCP # 162

--- DailyCodingProblem/162_Square_Shortest_Unique_Prefix.py
+++ DailyCodingProblem/162_Square_Shortest_Unique_Prefix.py
@@ -70,7 +70,71 @@ def get_unique_prefixes(words):
     return unique_prefixes
 
 
+# better implementation using Trie data structures
+class Node:
+    def __init__(self, val=None):
+        self.value = val  # Node value
+        self.children = {}  # all children of this node, with letters as keys
+        self.count = 0  # count of the number of times this letter appeared , root node's None implicitly part of every word's start
+        self.is_complete = False  # denotes if the node represents a complete word
+
+    def add_child(self, val):
+        # First update counter of this node
+        self.count += 1
+
+        # then check if val does not exist as a child:
+        if val not in self.children:
+            # create it if it does not exist
+            self.children[val] = Node(val)
+
+        # return the child node
+        return self.children[val]
+
+    def __repr__(self):
+        return "{}:{}".format(self.value, list(self.children.keys()))
+
+class Trie:
+    def __init__(self):
+        self.root = Node()  # root node is empty
+
+    def add_word(self, word):
+        curr_node = self.root
+        for letter in word:
+            curr_node = curr_node.add_child(letter)
+
+        curr_node.is_complete = True
+        curr_node.count+=1 #
+
+    def number_of_words(self):
+        return self.root.count
+
+    def unique_prefixes(self, word_list):
+        unique_prefixes = []
+        for word in word_list:
+            prefix = ""
+            curr_node = self.root
+            for letter in word:
+                prefix = prefix + letter
+                curr_node = curr_node.children[letter]
+                if curr_node.count == 1:
+                    unique_prefixes.append(prefix)
+                    break
+
+
+
+        return unique_prefixes
+
+def get_unique_prefixes_redux(words):
+    my_trie = Trie()
+
+    for word in words:
+        my_trie.add_word(word)
+
+    return my_trie.unique_prefixes(words)
+
 if __name__ == '__main__':
     print(get_unique_prefixes(['dog', 'cat', 'apple', 'apricot', 'fish']))
     print(get_unique_prefixes(['dog', 'cat', 'apple', 'apricot', 'fish', 'car']))
 
+    print(get_unique_prefixes_redux(['dog', 'cat', 'apple', 'apricot', 'fish']))
+    print(get_unique_prefixes_redux(['dog', 'cat', 'apple', 'apricot', 'fish', 'car']))
