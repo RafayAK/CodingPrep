4c91d34581dfadd9acd6d4fa73092417edc87034
Rafay Khan <rafayak16@gmail.com>
2020-07-19 19:37:54 +0500

    add initial implementation

--- DailyCodingProblem/166_Uber_Implement_2D_Iterator_Class.py
+++ DailyCodingProblem/166_Uber_Implement_2D_Iterator_Class.py
@@ -11,3 +11,36 @@ For example, given the input [[1, 2], [3], [], [4, 5, 6]], calling next() repeat
 
 Do not use flatten or otherwise clone the arrays. Some of the arrays can be empty.
 """
+
+class ArrayIterator:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self._curr_row = 0
+        self._curr_col = 0
+
+    def next(self):
+        if self.has_next():
+            return_val = self.matrix[self._curr_row][self._curr_col]
+            self._curr_col+=1
+            if self._curr_col == len(self.matrix[self._curr_row]):
+                self._curr_row+=1
+                self._curr_col=0
+            return return_val
+        else:
+            raise Exception(IndexError)
+
+    def has_next(self):
+        if self._curr_row < len(self.matrix) and len(self.matrix[self._curr_row]) == 0:
+            self._curr_row += 1  # skip this row
+        return self._curr_row < len(self.matrix) and self._curr_col < len(self.matrix[self._curr_row])
+
+
+
+if __name__ == '__main__':
+    test = ArrayIterator([[1, 2], [3], [], [4, 5, 6]])
+
+    while test.has_next():
+        print(test.next())
+
+
+
