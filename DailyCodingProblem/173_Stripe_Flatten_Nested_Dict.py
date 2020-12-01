ea4174e17d04d7721f48d6cfccbf337bfe86e965
Rafay Khan <rafayak16@gmail.com>
2020-12-01 13:11:44 +0500

    Solve Daily Coding Problem # 173

---
+++ DailyCodingProblem/173_Stripe_Flatten_Nested_Dict.py
@@ -0,0 +1,65 @@
+"""
+This problem was asked by Stripe.
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+"""
+
+add_to_basekey = lambda basekey, key: basekey + "." + key
+
+def get_keys(d:dict):
+    try:
+        return list(d.keys())
+    except AttributeError:
+        return None
+
+def flatten_dict(dictionary:dict):
+    flat_dict = {}
+
+    def helper(basekey, values):
+        if get_keys(values):
+            # if keys return successfully that means "values" to this "key" were a dict
+            for k in get_keys(values):
+                helper(add_to_basekey(basekey, k), values[k])
+        else:
+            # values was not a dict for this key
+            flat_dict[basekey] = values
+
+    for key in get_keys(d):
+        helper(key, dictionary[key])
+
+    return flat_dict
+
+
+if __name__ == '__main__':
+    print(add_to_basekey("hello", "world"))
+    d = {
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+            }
+        }
+    }
+
+    print(flatten_dict(d))
