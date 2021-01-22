73dc9995b11e8159074a94077911d4fd34717d0a
Rafay Khan <rafayak16@gmail.com>
2021-01-22 12:31:51 +0500

    Add second solution for DCP #180

--- DailyCodingProblem/180_Google_Interleave_Stack.py
+++ DailyCodingProblem/180_Google_Interleave_Stack.py
@@ -12,7 +12,7 @@ If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
 Hint: Try working backwards from the end state.
 """
 from queue import SimpleQueue
-
+from math import ceil
 
 class Stack:
     def __init__(self):
@@ -56,6 +56,58 @@ class Stack:
             push_from_queue_to_stack(num_of_times=i)
 
 
+    def interleave_stack_redux(self):
+        """
+        This follows a different approach
+        Thing to note in a stack of [1,2,3,4,5] the resultant
+        interleaved stack is the result of the stack [3,2,1] and the queue (5, 4)
+        If we do the following:
+        [3, 2, 1], (5, 4)
+        [3, 2, 1], (5, 4, 1)
+        [3, 2], (4, 1, 5)
+        [3, 2], (4, 1, 5)
+        [3], [4, 1, 5, 2]
+        [3], [1, 5, 2, 4]
+        [], [1,5,2,4,3]
+        [1, 5, 2, 4, 3]
+
+        First to get to the state [3, 2, 1], (5, 4)
+        [1, 2, 3, 4, 5]
+        put all stack to queue [], (5,4,3,2,1)
+        pop len(stack)//2 from queue back into queue ->  (3,2,1,5,4)
+        pop ceil(stack/2) from queue into stack -> [3, 2, 1], (5,4)
+        Now do the above steps
+
+        """
+        queue = SimpleQueue()
+        size_of_stack = self.size()
+        # step 1: move all elements to queue
+        while self._stack:
+            queue.put(self.pop())
+
+        # step 2: Rotate queue for size_of_stack//2 elements
+        for _ in range(size_of_stack//2):
+            queue.put(queue.get())
+
+        # step 3: put ceil(size_of_stack/2) elements back to queue
+        for _ in range(ceil(size_of_stack/2)):
+            self.push(queue.get())
+
+        # step 4: interleave the stack
+        for _ in range(size_of_stack//2):
+            # pop from stack then get from queue
+            queue.put(self.pop())
+            queue.put(queue.get())
+
+        if self._stack:
+            # if any remaining pop to queue
+            queue.put(self.pop())
+
+        # step 5: pop to stack
+        while not queue.empty():
+            self.push(queue.get())
+
+
     def __repr__(self):
         stack_str = ""
 
@@ -74,7 +126,8 @@ if __name__ == '__main__':
     print(stack_1)
 
     print("\nAfter interleaving:")
-    stack_1.interleave_stack()
+    # stack_1.interleave_stack()
+    stack_1.interleave_stack_redux()
     print(stack_1)
 
     print("\n-----------\n")
@@ -85,7 +138,8 @@ if __name__ == '__main__':
     print(stack_2)
 
     print("\nAfter interleaving:")
-    stack_2.interleave_stack()
+    # stack_2.interleave_stack()
+    stack_2.interleave_stack_redux()
     print(stack_2)
 
 
